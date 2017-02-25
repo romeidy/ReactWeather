f282f9df194567af885fcd8f34a0010824a20f7f
romeidy <jay.wardhana@hotmail.com>
2017-02-25 14:48:32 +0700

    fix heroku port

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ var app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(function (res, req, next){
+  if(req.headers['x-fowarded-proto'] === 'http'){
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+})
+
 app.use(express.static('public'));
 
 app.listen(PORT, function() {
