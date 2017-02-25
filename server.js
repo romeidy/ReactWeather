5df8477b4a23b065c08aa8e0c563a8006154b24c
romeidy <jay.wardhana@hotmail.com>
2017-02-25 15:07:35 +0700

    fix https

--- server.js
+++ server.js
@@ -4,14 +4,6 @@ var app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(function (res, req, next){
-  if(req.headers['x-fowarded-proto'] === 'http'){
-    next();
-  } else {
-    res.redirect('http://' + req.hostname + req.url);
-  }
-})
-
 app.use(express.static('public'));
 
 app.listen(PORT, function() {
