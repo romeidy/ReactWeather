a37e4ebf7bdb6e7525659c452e6c5c1617d18931
romeidy <jay.wardhana@hotmail.com>
2017-02-25 15:22:37 +0700

    fix port #6

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next){
-  if (req.headers['X-Forwarded-Proto'] === 'http'){
+  if (req.headers['X-Forwarded-Proto'] === 'https'){
     res.redirect('http://' + req.hostname + req.url);
-  }else {  
+  }else {
     next();
   }
 });
